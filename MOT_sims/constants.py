036b460fe5e88e4fd942495b3b9a3f01b5065778
Johan Palm <johan.palm2000@gmail.com>
2026-02-24 11:48:18 +0100

    The constants in the experiment used

---
+++ MOT_sims/constants.py
@@ -0,0 +1,25 @@
+import scipy.constants as const
+import numpy as np
+
+# 1. Define all constants globally so all CPU cores can see them
+frq_real = 603976506.6e6 * 2 * np.pi
+gamma_real = 61.4e6
+kmag_real = frq_real / const.c
+muB_real = const.physical_constants["Bohr magneton"][0]
+mass_real = const.value('atomic mass constant') * 88
+alpha_real = 0.4  # in T/m
+
+# Natural units for computation
+gamma = 1
+kmag = 1
+muB = 1
+mass = mass_real * gamma_real / const.hbar / kmag_real**2
+alpha = alpha_real * muB_real / (gamma_real * kmag_real * const.hbar)
+det = -2.1 * gamma
+s = 2
+transform = True
+
+rscale = np.array([2, 2, 2]) / alpha
+roffset = np.array([0.0, 0.0, 0.0])
+vscale = np.array([0.1, 0.1, 0.1])
+voffset = np.array([0.0, 0.0, 0.0])
